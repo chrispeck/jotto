828115c2ad27a71005ec07d87b9f20101e9f989e
Chris Peck <peckpeckpeck@gmail.com>
2012-12-05 16:30:02 -0500

    The beginnings of a game class after first hour of pair programming with Cecilia

--- lib/jotto/game.rb
+++ lib/jotto/game.rb
@@ -1,6 +1,35 @@
 module Jotto
   class Game
 
+	def initialize (max_guess_count, dict, secret)
+		@guess_count = 0 
+		@max_guess_count = max_guess_count
+		@secret = ''
+		@dict = dict
+		@secret = secret
+	end
+
+	def guess_count
+		return @guess_count 	
+	end
+	def max_guess_count
+		return @max_guess_count
+	end
+	def secret
+		return @secret
+	end
+	def dict
+		return @dict
+	end
+
+
+	def guess (something)
+		@guess_count += 1
+	end
+	def new_game ()
+		@guess_count = 0
+		@secret = @dict
+	end
   end
 end
 
